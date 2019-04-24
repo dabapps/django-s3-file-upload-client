73b91d2512129c1075bb8a0e7776597ba3d23af6
Jake 'Sid' Smith <jake@dabapps.com>
2019-04-24 10:33:48 +0100

    Refactor mocks to create Promise.all mock

---
+++ tests/helpers/mock-promise-all.ts
@@ -0,0 +1,45 @@
+import { MockPromise, MockPromiseCall } from './mock-promise';
+
+export type MockPromiseAllFunction = (
+  promises: ReadonlyArray<Promise<any>>
+) => Promise<ReadonlyArray<any>>;
+
+export interface MockPromiseAllObject {
+  calls: ReadonlyArray<MockPromiseCall>;
+  clear: () => void;
+}
+
+export type MockPromiseAll = MockPromiseAllFunction & MockPromiseAllObject;
+
+const mockPromiseAllObject: MockPromiseAllObject = {
+  calls: [],
+  clear,
+};
+
+const mockPromiseAll = Object.assign(
+  mockPromiseAllFunction,
+  mockPromiseAllObject
+);
+
+function mockPromiseAllFunction(...args: any[]) {
+  const call: MockPromiseCall = {
+    arguments: args,
+    thenCalls: [],
+    catchCalls: [],
+  };
+
+  mockPromiseAll.calls = [...mockPromiseAll.calls, call];
+
+  return new MockPromise(call);
+}
+
+function clear() {
+  mockPromiseAll.calls.forEach(call => {
+    call.thenCalls = [];
+    call.catchCalls = [];
+  });
+
+  mockPromiseAll.calls = [];
+}
+
+export { mockPromiseAll };
---
+++ tests/helpers/mock-promise.ts
@@ -0,0 +1,39 @@
+export interface MockPromiseChainCall {
+  arguments: any[];
+}
+
+export interface MockPromiseCall {
+  arguments: any[];
+  thenCalls: MockPromiseChainCall[];
+  catchCalls: MockPromiseChainCall[];
+}
+
+export class MockPromise extends Promise<any> {
+  private call: MockPromiseCall;
+
+  public constructor(call: MockPromiseCall) {
+    super(resolve => {
+      return resolve();
+    });
+
+    this.call = call;
+  }
+
+  public then(...args: any[]) {
+    const call: MockPromiseChainCall = {
+      arguments: args,
+    };
+
+    this.call.thenCalls.push(call);
+    return new MockPromise(this.call);
+  }
+
+  public catch(...args: any[]) {
+    const call: MockPromiseChainCall = {
+      arguments: args,
+    };
+
+    this.call.catchCalls.push(call);
+    return new MockPromise(this.call);
+  }
+}
